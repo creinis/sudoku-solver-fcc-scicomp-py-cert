641ac6c3f1dd8cd2fd9d327d74916c89b911c25e
creinis <115181527+creinis@users.noreply.github.com>
2024-08-02 14:02:58 +0200

    feat: create Board class.

---
+++ lecture.py
@@ -0,0 +1,32 @@
+# Sudoku Solver
+
+# Step 1
+
+# In this project, you will learn about classes and objects by building a sudoku puzzle solver.
+
+# In Python, a class is a blueprint for creating objects. 
+# Objects created from a class are instances of that class. You can create a class using this syntax:
+
+# class ClassName:
+
+# First, you will create a 9x9 board by using classes and then populate it with the puzzle values.
+
+# Begin by creating a Board class.
+
+class Board:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
